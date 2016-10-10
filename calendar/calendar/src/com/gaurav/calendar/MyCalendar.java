834a589474cda74a429ec2ceb33e6dde8cc79999
gaurav <gagu12ec@gmail.com>
2016-10-10 22:24:26 +0530

    try to use GregorianCalendar

---
+++ calendar/calendar/src/com/gaurav/calendar/MyCalendar.java
@@ -0,0 +1,45 @@
+package com.gaurav.calendar;
+
+public class MyCalendar extends java.util.Calendar {
+
+	public void add(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void computeFields() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void computeTime() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getGreatestMinimum(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getLeastMaximum(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getMaximum(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getMinimum(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void roll(int arg0, boolean arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
